b73a9c56b2393b208f19751e8c7f1f9d300c256e
KVijay2 <vijaykarunanidhi4@gmail.com>
2023-04-02 18:03:17 +0530

    Vijay

--- src/assets/index.js
+++ src/assets/index.js
@@ -9,12 +9,10 @@ import close from "./close.svg";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
 import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
@@ -47,10 +45,8 @@ export {
   nextjs,
   git,
   html,
-  figma,
   javascript,
   mongodb,
-  nodejs,
   reactjs,
   redux,
   tailwind,
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,16 +33,16 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_d8jwimk",
+        "template_mkcvlyk",
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Vijay K",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "vijaykarunanidhi4@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        "rQi1SkynVr5qOfAEt"
       )
       .then(
         () => {
